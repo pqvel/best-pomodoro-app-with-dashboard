793bf540b7f90fe4c0606cefdd7acdf87e288940
Pavel Salianik <solyqnik@gmail.com>
2024-03-07 23:27:21 +0300

    refactor AddTodo.tsx

--- src/components/dashboard/AddTodo.tsx
+++ src/components/dashboard/AddTodo.tsx
@@ -1,13 +1,9 @@
 import { FC } from "react";
-import { useForm } from "react-hook-form";
-import Svg from "../ui/Svg";
+import { Svg, Button } from "../ui";
 import TodoForm from "../forms/TodoForm";
 import { useAppDispatch, useAppSelector } from "../../core/redux/app/hooks";
 import { setTodoPopupSectionId } from "../../core/redux/slices/popupSlice";
-import { createTodo } from "../../core/redux/slices/dashboardSlice";
-import { TodoModel } from "../../core/models/TodoModel";
 
-// тултип это подсказка, нужно поменять названия
 type AddTodoProps = {
   dashboardId: string;
   sectionId: string;
@@ -15,17 +11,17 @@ type AddTodoProps = {
 
 const AddTodo: FC<AddTodoProps> = ({ dashboardId, sectionId }) => {
   const dispatch = useAppDispatch();
-  const todoPopupSectionId = useAppSelector(
-    (state) => state.popup.todoPopupSectionId
-  );
+  const { todoPopupSectionId } = useAppSelector((state) => state.popup);
 
-  const { handleSubmit } = useForm();
+  const openTodoForm = () => {
+    dispatch(setTodoPopupSectionId(sectionId));
+  };
 
   if (sectionId !== todoPopupSectionId) {
     return (
-      <AddTodoButton
-        openTodoForm={() => dispatch(setTodoPopupSectionId(sectionId))}
-      />
+      <Button onClick={openTodoForm}>
+        <Svg width={20} height={20} iconId="icon-plus" /> Добавить задачу
+      </Button>
     );
   }
 
@@ -37,16 +33,5 @@ const AddTodo: FC<AddTodoProps> = ({ dashboardId, sectionId }) => {
     />
   );
 };
-
-type AddTodoButtonProps = {
-  openTodoForm: () => void;
-};
-
-const AddTodoButton: FC<AddTodoButtonProps> = ({ openTodoForm }) => (
-  <button className="flex items-center gap-3" onClick={openTodoForm}>
-    <Svg width={20} height={20} iconId="icon-plus" />
-    Добавить задачу
-  </button>
-);
 
 export default AddTodo;
