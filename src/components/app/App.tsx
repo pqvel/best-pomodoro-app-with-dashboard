a9fbc121984589f6b1b0ac94a43a294d1856484f
Pavel Salianik <solyqnik@gmail.com>
2024-07-16 17:48:12 +0300

    add hashrouter

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, FC } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
+
 import TimerPage from "../pages/TimerPage";
 import TodosPage from "../pages/Todos";
 import StatsPage from "../pages/Stats";
@@ -12,13 +13,15 @@ const App: FC = () => {
   }, []);
 
   return (
-    <Routes>
-      <Route path="/" element={<TimerPage />} />
-      <Route path="/todos" element={<TodosPage />} />
-      <Route path="/stats" element={<StatsPage />} />
-      <Route path="/info" element={<InfoPage />} />
-      <Route path="/account" element={<AccountPage />} />
-    </Routes>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<TimerPage />} />
+        <Route path="/todos" element={<TodosPage />} />
+        <Route path="/stats" element={<StatsPage />} />
+        <Route path="/info" element={<InfoPage />} />
+        <Route path="/account" element={<AccountPage />} />
+      </Routes>
+    </HashRouter>
   );
 };
 
